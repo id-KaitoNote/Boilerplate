dc5c052221aa26e5cc25e2e3500df15205d65511
id-KaitoNote <id_not@au.com>
2022-02-27 19:14:23 +0900

    stylelint setting

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,20 @@ const path = require('path')
 const configPath = require('./config-path')
 
 module.exports = {
-    mode: 'production',
-    entry: './src/assets/scripts/main.js',
-    module: {
-        rules: [
-            {
-                exclude: /node_modules/,
-                use: {
-                    loader:'babel-loader'
-                },
-            }
-        ]
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.join(__dirname, `${configPath.dirs.js.dist}`)
-    }
+  mode: 'production',
+  entry: './src/assets/scripts/main.js',
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, `${configPath.dirs.js.dist}`),
+  },
 }
